eabb67f33e242429a0681752f00c6f7a54b91a53
Martin Garcia Hervas <punighervas@gmail.com>
2023-08-15 16:38:52 -0300

    Finished

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -9,6 +9,18 @@ var traverseDomAndCollectElements = function (matchFunc, startEl) {
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
+  function traverse(startEl) {
+    if (matchFunc(startEl)) resultSet.push(startEl);
+
+    for (var i = 0; i < startEl.children.length; i++) {
+      traverse(startEl.children[i]);
+    }
+  }
+
+  // Inicia la recursión desde el elemento de inicio
+  traverse(startEl);
+
+  return resultSet;
 };
 
 // Detecta y devuelve el tipo de selector
@@ -16,6 +28,15 @@ var traverseDomAndCollectElements = function (matchFunc, startEl) {
 
 var selectorTypeMatcher = function (selector) {
   // tu código aquí
+  if(selector[0] === "#") {
+    return "id";
+  } else if(selector.startsWith(".")) {
+    return "class";
+  } else if(selector.includes(".")){
+    return "tag.class";
+  } else {
+    return "tag";
+  }
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -23,14 +44,23 @@ var selectorTypeMatcher = function (selector) {
 // parametro y devuelve true/false dependiendo si el elemento
 // matchea el selector.
 
-var matchFunctionMaker = function (selector) {
+var matchFunctionMaker = function (selector) { 
   var selectorType = selectorTypeMatcher(selector);
-  var matchFunction;
-  if (selectorType === "id") {
+  var matchFunction = function (element){
+    if (selectorType === "id") {
+      return element.id === selector.substring(1); //el metodo substring() elimina los caracteres de un string q le indicas // le elimino el #
   } else if (selectorType === "class") {
+      return element.classList.contains(selector.substring(1));  
   } else if (selectorType === "tag.class") {
+      if(element.tagName && element.tagName.toLowerCase() === selector.split(".")[0].toLowerCase() && element.classList.contains(selector.split(".")[1])){
+        return true;
+      } else {
+        return false;
+      };
   } else if (selectorType === "tag") {
+    return element.tagName && element.tagName.toLowerCase() === selector.toLowerCase();
   }
+}
   return matchFunction;
 };
 
