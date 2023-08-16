61ebdb2b37cb68463dab09f9c906c945a951c422
Martin Garcia Hervas <punighervas@gmail.com>
2023-08-16 15:05:02 -0300

    Finished

---
+++ 04-Ajax/homework/index.js
@@ -0,0 +1,39 @@
+
+$("#boton").on("click", function () {
+$.get("http://localhost:5000/amigos", (data) => { 
+
+for(let i = 0; i < data.length; i++){
+  let info = document.createElement("li");
+  info.textContent = data[i].name
+  info.id = data[i].id
+  $("#lista").append(info);
+}
+
+})
+});
+
+$("#search").on("click", function () {
+    $.get("http://localhost:5000/amigos/" + input.value, (data) => {
+
+    let info = document.createElement("p");
+    info.textContent = data.name;
+    $("#amigo").append(info);
+    
+    })
+    })
+
+$("#delete").on("click", function () {
+    $.ajax({
+        type: "DELETE",
+        url: "http://localhost:5000/amigos/" + inputDelete.value,
+          success: (data) => {
+            console.log(data);
+
+              let info = document.createElement("p");
+              info.textContent = `Tu amigo borrado fue borrado con exito`;
+              $("#success").append(info);
+
+              $(`#${inputDelete.value}`).remove();
+        }
+    })
+})
