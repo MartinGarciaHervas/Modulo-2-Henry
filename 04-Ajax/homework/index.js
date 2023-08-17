f83cbcf0a75d1414f799382f82064a4b322035d0
Martin Garcia Hervas <punighervas@gmail.com>
2023-08-17 10:35:20 -0300

    Finished

--- 04-Ajax/homework/index.js
+++ 04-Ajax/homework/index.js
@@ -1,59 +1,106 @@
-$("#boton").on("click", function () {
-$.get("http://localhost:5000/amigos", (data) => { 
+// $("#boton").on("click", function () {
 
-for(let i = 0; i < data.length; i++){
-  let info = document.createElement("li");
-  info.textContent = data[i].name
-  info.id = data[i].id
-  $("#lista").append(info);
+//   $("#lista").empty();  // esto lo q hace es vaciar la lista si apretamos el click, cosa de que no se vuelva a cargar la lista de amigos de nuevo
+
+//   $.get("http://localhost:5000/amigos", (data) => {
+//     data.map(amigo => {
+//       let info = document.createElement("li");
+//       info.innerHTML = amigo.name;
+
+//       $("#lista").append(info)
+//     });
+//   })
+// });
+
+// $("#search").on("click", function () {
+//   $("#amigo").empty();
+//   let id = $("#input").val();
+
+//   if (id) {
+//     $.get(`http://localhost:5000/amigos/${id}`, (data) => {
+
+//       let info = document.createElement("p");
+//       info.textContent = data.name;
+//       $("#amigo").append(info);
+
+//     })
+//   }
+// })
+
+// $("#delete").on("click", function () {
+//   $("#success").empty();
+//   let id = $("#inputDelete").val();
+
+//   if (id) {
+//     $.ajax({
+//       type: "DELETE",
+//       url: `http://localhost:5000/amigos/${id}`,
+//       success: () => {
+//         let info = document.createElement("p");
+//         info.textContent = `Tu amigo borrado fue borrado con exito`;
+//         $("#success").append(info);
+
+//         $(`#${id}`).remove();
+//       }
+//     })
+//   }
+// })
+
+
+// lo ideal es crear las funciones aparte, de esa manera nos queda separado y nos permite llamarla cuando necesitemos, aparte de ser mas 
+// ordenado
+// esto permite enviar las funciones a otro archivo, y que solo las acciones queden en un archivo :)
+// seria algo asi:
+
+let friendsHandler = function () {
+  $("#lista").empty();
+
+  $.get("http://localhost:5000/amigos", (data) => {
+    data.map(amigo => {
+      let info = document.createElement("li");
+      info.innerHTML = amigo.name;
+
+      $("#lista").append(info)
+    })
+  }
+  )
 }
 
-})
-});
+let searchHandler = function () {
+  $("#amigo").empty();
+  let id = $("#input").val();
 
-$("#search").on("click", function () {
-    $.get("http://localhost:5000/amigos/" + input.value, (data) => {
+  if (id) {
+    $.get(`http://localhost:5000/amigos/${id}`, (data) => {
 
-    let info = document.createElement("p");
-    info.textContent = data.name;
-    $("#amigo").append(info);
-    
+      let info = document.createElement("p");
+      info.textContent = data.name;
+      $("#amigo").append(info);
+
     })
-    })
+  }
+}
 
-$("#delete").on("click", function () {
+let deleteHandler = function () {
+  $("#success").empty();
+  let id = $("#inputDelete").val();
+
+  if (id) {
     $.ajax({
-        type: "DELETE",
-        url: "http://localhost:5000/amigos/" + inputDelete.value,
-          success: (data) => {
-            console.log(data);
-
-              let info = document.createElement("p");
-              info.textContent = `Tu amigo borrado fue borrado con exito`;
-              $("#success").append(info);
-
-              $(`#${inputDelete.value}`).remove();
-        }
+      type: "DELETE",
+      url: `http://localhost:5000/amigos/${id}`,
+      success: () => {
+        let info = document.createElement("p");
+        info.textContent = `Tu amigo borrado fue borrado con exito`;
+        $("#success").append(info);
+        friendsHandler(); //aca puedo llamar de nuevo a la funcion que me muestra los amigos, asi se actualiza la lista sin el que acabo de eliminar
+      }
     })
-})
+  }
+}
 
-$("#agregar").on("click", function() {
-$.post( "http://localhost:5000/amigos", { name: "John", time: "2pm" } )
-})
+$("#boton").on("click", friendsHandler);
 
+$("#search").on("click", searchHandler);
 
-
-// $("#agregar").on("click", function() {
-//   $.ajax({
-//     type: "POST",
-//     url: "http://localhost:5000/amigos",
-//     data: {
-//       name: $("#nombre").val(),
-//       apellido: $("#apellido").val(),
-//       email: $("#mail").val()
-//         },
-//     success: (data) => {
-//       console.log(data);
-//     }
-//   });
-// })
+$("#delete").on("click", deleteHandler);
