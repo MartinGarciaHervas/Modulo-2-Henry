fc2cb543f658f03b9840f5aae15dc15e536fb926
Martin Garcia Hervas <punighervas@gmail.com>
2023-08-22 10:10:13 -0300

    Finished

--- 06-React-Intro/homework/02 - Integration/src/components/Card.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Card.jsx
@@ -1,13 +1,14 @@
 export default function Card(props) {
+   const {name, status, species, origin, gender, image, onClose} = props;
    return (
       <div>
-         {/* <button onClick={}>X</button>
-         <h2></h2>
-         <h2></h2>
-         <h2></h2>
-         <h2></h2>
-         <h2></h2>
-         <img src={} alt='' /> */}
+         <button onClick={onClose}>X</button>
+         <h2>Nombre:{name}</h2>
+         {/* <h2>Status:{status}</h2> */}
+         <h2>Species:{species}</h2>
+         {/* <h2>Origin:{origin}</h2> */}
+         <h2>Gender:{gender}</h2>
+         <img src={image} alt={name} />
       </div>
    );
 }
